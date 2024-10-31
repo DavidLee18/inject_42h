f96f18ab04abead10ccdd0ea69612f001156d321
David Lee <djwodus@gmail.com>
2024-10-31 17:55:02 +0900

    can now update modified datetime, updater name, and changed file name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use chrono::DateTime;
+pub mod parser;
+
+use crate::parser::{_42Header, _42header};
+use chrono::{DateTime, FixedOffset};
 use clap::Parser;
-use std::{fs, time::UNIX_EPOCH};
+use std::path::Path;
+use std::{fs, io, time::UNIX_EPOCH};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -14,20 +18,34 @@ struct Args {
     path: String,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
     let args = Args::parse();
     let content = fs::read_to_string(&args.path)?;
-    let header = _42(
-        args.name,
-        args.email,
-        args.path
-            .split('/')
-            .last()
-            .expect("file name cannot be retreived"),
-        &args.path,
-    );
-    fs::write(&args.path, header + &content)?;
-    Ok(())
+    match _42header(&content) {
+        Ok((c, header)) => {
+            fs::write(&args.path, _42_replace(header, args.name, Path::new(&args.path), args.path
+                .split('/')
+                .last()
+                .expect("file name cannot be retrieved")) + c)?;
+            Ok(())
+        }
+        Err(e) => match e {
+            nom::Err::Incomplete(_) => Err(io::Error::new(io::ErrorKind::InvalidData, "Incomplete")),
+            nom::Err::Error(nom::error::Error { input: c, .. }) | nom::Err::Failure(nom::error::Error { input: c, .. })=> {
+                let header = _42(
+                    args.name,
+                    args.email,
+                    args.path
+                        .split('/')
+                        .last()
+                        .expect("file name cannot be retrieved"),
+                    &args.path,
+                );
+                fs::write(&args.path, header + c)?;
+                Ok(())
+            }
+        }
+    }
 }
 
 fn _42(name: String, email: String, file: &str, path: &str) -> String {
@@ -43,6 +61,7 @@ fn _42(name: String, email: String, file: &str, path: &str) -> String {
     );
     res.push_str("/*   ");
     res.push_str(&file);
+    assert!(51 >= file.len());
     res.push_str(" ".repeat(51 - file.len()).as_str());
     res.push_str(":+:      :+:    :+:   */\n");
     res.push_str(
@@ -53,6 +72,7 @@ fn _42(name: String, email: String, file: &str, path: &str) -> String {
     res.push_str(" <");
     res.push_str(&email);
     res.push_str(">");
+    assert!(40 >= name.len() + email.len());
     res.push_str(" ".repeat(40 - name.len() - email.len()).as_str());
     res.push_str("+#+  +:+       +#+        */\n");
     res.push_str(
@@ -65,10 +85,12 @@ fn _42(name: String, email: String, file: &str, path: &str) -> String {
     let c_dur = created
         .duration_since(UNIX_EPOCH)
         .expect("conversion failed: created");
-    let created = DateTime::from_timestamp_nanos(c_dur.as_nanos().try_into().unwrap());
+    let created = DateTime::from_timestamp_nanos(c_dur.as_nanos().try_into().unwrap())
+        .with_timezone(&FixedOffset::east_opt(9 * 3600).unwrap());
     res.push_str(&created.format("%Y/%m/%d %H:%M:%S").to_string());
     res.push_str(" by ");
     res.push_str(&name);
+    assert!(18 >= name.len());
     res.push_str(" ".repeat(18 - name.len()).as_str());
     res.push_str("#+#    #+#             */\n");
 
@@ -77,11 +99,79 @@ fn _42(name: String, email: String, file: &str, path: &str) -> String {
     let m_dur = modified
         .duration_since(UNIX_EPOCH)
         .expect("conversion failed: modified");
-    let modified = DateTime::from_timestamp_nanos(m_dur.as_nanos().try_into().unwrap());
+    let modified = DateTime::from_timestamp_nanos(m_dur.as_nanos().try_into().unwrap())
+        .with_timezone(&FixedOffset::east_opt(9 * 3600).unwrap());
     res.push_str(&modified.format("%Y/%m/%d %H:%M:%S").to_string());
     res.push_str(" by ");
     res.push_str(&name);
+    assert!(17 >= name.len());
     res.push_str(" ".repeat(17 - name.len()).as_str());
+    res.push_str("###   ########.fr       */\n");
+    res.push_str(
+        "/*                                                                            */\n",
+    );
+    res.push_str(
+        "/* ************************************************************************** */\n",
+    );
+
+    res
+}
+
+fn _42_replace(header: _42Header, updater_name: String, path: &Path, file_name: &str) -> String {
+    let mut res = String::new();
+    res.push_str(
+        "/* ************************************************************************** */\n",
+    );
+    res.push_str(
+        "/*                                                                            */\n",
+    );
+    res.push_str(
+        "/*                                                        :::      ::::::::   */\n",
+    );
+    res.push_str("/*   ");
+    res.push_str(file_name);
+    assert!(51 >= file_name.len());
+    res.push_str(" ".repeat(51 - file_name.len()).as_str());
+    res.push_str(":+:      :+:    :+:   */\n");
+    res.push_str(
+        "/*                                                    +:+ +:+         +:+     */\n",
+    );
+    res.push_str("/*   By: ");
+    res.push_str(&header.name);
+    res.push_str(" <");
+    res.push_str(&header.email);
+    res.push_str(">");
+    assert!(40 >= header.name.len() + header.email.len());
+    res.push_str(
+        " ".repeat(40 - header.name.len() - header.email.len())
+            .as_str(),
+    );
+    res.push_str("+#+  +:+       +#+        */\n");
+    res.push_str(
+        "/*                                                +#+#+#+#+#+   +#+           */\n",
+    );
+
+    res.push_str("/*   Created: ");
+    let meta = fs::metadata(path).expect("metadata on this path does not exist");
+    res.push_str(&header.created.format("%Y/%m/%d %H:%M:%S").to_string());
+    res.push_str(" by ");
+    res.push_str(&header.name);
+    assert!(18 >= header.name.len());
+    res.push_str(" ".repeat(18 - header.name.len()).as_str());
+    res.push_str("#+#    #+#             */\n");
+
+    res.push_str("/*   Updated: ");
+    let modified = meta.modified().expect("modified time not available");
+    let m_dur = modified
+        .duration_since(UNIX_EPOCH)
+        .expect("conversion failed: modified");
+    let modified = DateTime::from_timestamp_nanos(m_dur.as_nanos().try_into().unwrap())
+        .with_timezone(&FixedOffset::east_opt(9 * 3600).unwrap());
+    res.push_str(&modified.format("%Y/%m/%d %H:%M:%S").to_string());
+    res.push_str(" by ");
+    res.push_str(&updater_name);
+    assert!(17 >= updater_name.len());
+    res.push_str(" ".repeat(17 - updater_name.len()).as_str());
     res.push_str("###   ########.fr       */\n");
     res.push_str(
         "/*                                                                            */\n",
---
+++ src/parser.rs
@@ -0,0 +1,90 @@
+use chrono::{DateTime, FixedOffset, NaiveDateTime};
+use nom::bytes::complete::tag;
+use nom::character::complete::{alphanumeric1, anychar, newline, space1};
+use nom::multi::many_till;
+use nom::IResult;
+
+pub struct _42Header {
+    pub file_name: String,
+    pub name: String,
+    pub email: String,
+    pub created: DateTime<FixedOffset>,
+    pub author_name: String,
+    pub updated: DateTime<FixedOffset>,
+    pub updater_name: String,
+}
+
+pub fn _42header(s: &str) -> IResult<&str, _42Header> {
+    let (s, _) =
+        tag("/* ************************************************************************** */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) =
+        tag("/*                                                                            */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) =
+        tag("/*                                                        :::      ::::::::   */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) = tag("/*   ")(s)?;
+    let (s, (file_name, _)): (&str, (Vec<char>, &str)) = many_till(anychar, space1)(s)?;
+    let (s, _) = many_till(space1, tag(":+:      :+:    :+:   */"))(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) =
+        tag("/*                                                    +:+ +:+         +:+     */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) = tag("/*   By: ")(s)?;
+    let (s, name) = alphanumeric1(s)?;
+    let (s, _) = tag(" <")(s)?;
+    let (s, (email, _)): (&str, (Vec<char>, &str)) = many_till(anychar, tag(">"))(s)?;
+    let (s, _) = space1(s)?;
+    let (s, _) = tag("+#+  +:+       +#+        */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) =
+        tag("/*                                                +#+#+#+#+#+   +#+           */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) = tag("/*   Created: ")(s)?;
+    let (s, (created, _)): (&str, (Vec<char>, &str)) = many_till(anychar, tag(" by "))(s)?;
+    let (s, author_name) = alphanumeric1(s)?;
+    let (s, _) = space1(s)?;
+    let (s, _) = tag("#+#    #+#             */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) = tag("/*   Updated: ")(s)?;
+    let (s, (updated, _)): (&str, (Vec<char>, &str)) = many_till(anychar, tag(" by "))(s)?;
+    let (s, updater_name) = alphanumeric1(s)?;
+    let (s, _) = space1(s)?;
+    let (s, _) = tag("###   ########.fr       */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) =
+        tag("/*                                                                            */")(s)?;
+    let (s, _) = newline(s)?;
+    let (s, _) =
+        tag("/* ************************************************************************** */")(s)?;
+    let (s, _) = newline(s)?;
+
+    Ok((
+        s,
+        _42Header {
+            file_name: file_name.into_iter().collect(),
+            name: name.to_string(),
+            email: email.into_iter().collect(),
+            created: DateTime::from_naive_utc_and_offset(
+                NaiveDateTime::parse_from_str(
+                    &created.into_iter().collect::<String>(),
+                    "%Y/%m/%d %H:%M:%S",
+                )
+                .expect("not a valid datetime format"),
+                FixedOffset::east_opt(9 * 3600).unwrap(),
+            ),
+
+            author_name: author_name.to_string(),
+            updated: DateTime::from_naive_utc_and_offset(
+                NaiveDateTime::parse_from_str(
+                    &updated.into_iter().collect::<String>(),
+                    "%Y/%m/%d %H:%M:%S",
+                )
+                .expect("not a valid datetime format"),
+                FixedOffset::east_opt(9 * 3600).unwrap(),
+            ),
+            updater_name: updater_name.to_string(),
+        },
+    ))
+}
